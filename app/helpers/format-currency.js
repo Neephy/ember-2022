398c34c82a2016f167f4819c58b31188bfba782c
bras <22000754@etu.unicaen.fr>
2022-02-10 13:35:38 +0100

    fin td2

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -3,5 +3,5 @@ import {helper} from '@ember/component/helper';
 export default helper (function formatCurrency(params /*, named*/) {
   let [value, symbol] = params;
   //value = Math.round(value * 100) / 100;
-  return $(value), $(symbol);
+  return `${value}, ${symbol}`;
 });
